8dd2dd9290f8e2c3315c7ffe3d324de65fbbbefa
amulya2505 <38892462+amulya2505@users.noreply.github.com>
2022-03-02 12:48:41 +0530

    Create constant.js
    this file contains all the selectors which are used in the project

---
+++ tests/constant.js
@@ -0,0 +1,19 @@
+const selectors = taiko => {
+    return {
+      dropdown: taiko.dropDown({ id: 'dropdown_id' }),
+      textfield: taiko.textBox({ id: 'textbox_id' }),
+      sampletext: taiko.text('Suno India'),
+      closebutton: taiko.button('close', taiko.below(taiko.dropDown({ id: 'button1' }))),
+      samplelink: taiko.link('some link'),
+      radiobutton: taiko.radioButton({ id: 'others' }),
+      image: taiko.image({ alt: 'image_alt_text' }),
+      
+    
+    };
+  };
+
+  const getSelectors = async taiko => {
+    return selectors(taiko);
+  };
+
+  module.exports = getSelectors;
